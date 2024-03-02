29d7e9d028ca2eb97f2c00b8d0e4f1cb88821501
pinkxpink <kyperu07@gmail.com>
2024-03-02 23:50:21 +0200

    start

---
+++ WPFLearn/Models/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLearn.Models
+{
+    class Hotel
+    {
+        //add field for read class ReservationBook
+        private readonly ReservationBook _reservationBook;
+        //Init field Name for Hotel
+        public string Name { get; }
+        public Hotel(string name)
+        {
+            //init the reservationBook for this Hotel
+            _reservationBook = new ReservationBook();
+            Name = name;    
+        }
+    }
+}
